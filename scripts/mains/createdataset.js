16fa762668985df1717495ce521beb1cad4f9444
DanieleO-fenix <daniele.olivotti@fao.org>
2014-11-17 12:08:42 +0100

    Updated codelists and Contextsystem

--- scripts/mains/createdataset.js
+++ scripts/mains/createdataset.js
@@ -52,32 +52,25 @@ define(['module'], function (module) {
                                 url: 'json/fenix-ui-topmenu_config.json', active: "createdataset"
                             });
 
-                            E.init();
+                            E.init({
+                                codelists: "config/submodules/DSDEditor/CodelistsUNECA.json"
+                            }, function () {
+                                $('#DSDEditorContainer').hide();
+                            });
+
+                            var datasource = "CountrySTAT";
+                            var contextSys = "UNECA";
 
                             DE.init();
 
                             var uid = "";
 
                             window.setTimeout(function () {
-                                /*E.setColumns([
-                                    {"id": "CODE", "title": {"EN": "item"}, "key": true, "dataType": "code", "domain": {"codes": [
-                                        {"idCodeList": "ECO_Commodity"}
-                                    ]}, "subject": "item", "supplemental": null},
-                                    {"id": "YEAR", "title": {"EN": "y"}, "key": true, "dataType": "year", "domain": null, "subject": "time", "supplemental": null},
-                                    {"id": "NUMBER", "title": {"EN": "measure"}, "key": false, "dataType": "number", "subject": "value", "supplemental": null},
-                                    {"id": "CODE2", "title": {"EN": "area"}, "key": false, "dataType": "code", "domain": {"codes": [
-                                        {"idCodeList": "ECO_GAUL"}
-                                    ]}, "subject": "geo", "supplemental": null}
-                                ]);*/
-
-
-                                $('#DSDEditorContainer').hide();
                                 $('#DataEditorContainer').hide();
-
                             }, 2000);
 
                             document.body.addEventListener("fx.editor.finish", function (e) {
-                                console.log(e.detail.data);
+                                //console.log(e.detail.data);
                                 uid = e.detail.data.uid;
 
                                 $('#metadataEditorContainer').hide();
@@ -86,14 +79,14 @@ define(['module'], function (module) {
                             }, false);
 
                             $('body').on("columnEditDone.DSDEditor.fenix", function (e, p) {
-                                var newDSD = {"columns": p.payload};
+                                var newDSD = { "columns": p.payload };
                                 //E.updateDSD("dan3", null, newDSD);
-                                E.updateDSD(uid, null, newDSD);
+                                E.updateDSD(uid, null, newDSD, datasource, contextSys);
 
                                 $('#DSDEditorContainer').hide();
                                 $('#DataEditorContainer').show();
 
-                                DE.set({"dsd": newDSD  });
+                                DE.set({ "dsd": newDSD });
                             })
 
                             $('#createDatasetEnd').on('click', function () {
@@ -114,20 +107,24 @@ define(['module'], function (module) {
                                                 var verCL = col.domain.codes[0].version;
 
                                                 if (verCL)
-                                                    col.values = {codes: [
-                                                        {idCodeList: idCL, version: verCL}
-                                                    ]};
+                                                    col.values = {
+                                                        codes: [
+                                                            { idCodeList: idCL, version: verCL }
+                                                        ]
+                                                    };
                                                 else
-                                                    col.values = {codes: [
-                                                        {idCodeList: idCL}
-                                                    ]};
+                                                    col.values = {
+                                                        codes: [
+                                                            { idCodeList: idCL }
+                                                        ]
+                                                    };
                                                 col.values.codes[0].codes = [];
                                                 for (var i = 0; i < distincts[colId].length; i++) {
-                                                    col.values.codes[0].codes.push({code: distincts[colId][i]});
+                                                    col.values.codes[0].codes.push({ code: distincts[colId][i] });
                                                 }
                                             }
                                             else {
-                                                col.values = {timeList: distincts[colId]};
+                                                col.values = { timeList: distincts[colId] };
                                             }
                                         }
                                     }
@@ -135,11 +132,11 @@ define(['module'], function (module) {
                                 }
                                 //console.log(meta.dsd);
                                 /*console.log("data");
-                                console.log(data);*/
+                                 console.log(data);*/
 
 
                                 DE.updateData(uid, null, data, function () {
-                                    DE.updateDSD(uid, null, meta.dsd, function () {
+                                    DE.updateDSD(uid, null, meta.dsd, datasource, contextSys, function () {
                                         window.location.reload();
                                     });
                                 });
