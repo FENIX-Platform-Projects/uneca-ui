5c4bb0a03fcd08fe9b4e02a978a01fb5e20faf88
DanieleO-fenix <daniele.olivotti@fao.org>
2015-05-12 11:41:53 +0200

    Configuration files added

---
+++ config/submodules/datamng/config.js
@@ -0,0 +1,32 @@
+﻿/*global define*/
+define(['jquery', 'fx-submodules/config/baseConfig'],
+    function ($, config_base) {
+
+        'use strict';
+
+        //Use the following example to override properties:
+        //services.SERVICE_BASE_ADDRESS = "http://fenix.fao.org/d3s_dev2/msd";
+
+        /*var services = {
+    
+            TOP_MENU: {
+                url: 'json/fenix-ui-topmenu_config.json',
+                active: "createdataset"
+            },
+            SERVICE_BASE_ADDRESS: "http://fenix.fao.org/d3s_dev/msd"
+        };*/
+
+        var services = {};
+        $.extend(services, config_base);
+
+        services.DSD_EDITOR_CODELISTS = "config/submodules/DSDEditor/CodelistsUNECA.json";
+        services.DSD_EDITOR_CONTEXT_SYSTEM = "UNECA";
+
+
+        /*services.TOP_MENU = {
+            url: 'json/fenix-ui-topmenu_config.json',
+            active: "createdataset"
+        };*/
+
+        return services;
+    });
