cd1f13271b70a457957711589f3ed25df0867664
Francesca Rosa <francesca.rosa@fao.org>
2015-09-01 14:27:39 +0200

    dashboard configuration -population

--- config/profile/config.js
+++ config/profile/config.js
@@ -28,6 +28,330 @@ define(function () {
 
         },
 
+        "population": {
+            //data cube's uid
+            uid: "UNECA_Population",
+
+            //bridge configuration
+            bridge: {
+
+                type: "d3p"
+
+            },
+
+            /*
+             * in case bridge is WDS this is the cube metadata.
+             * if bridge is D3P this is ignored
+             * */
+            metadata: {},
+
+            items: [
+                {
+                    //Time series
+                    id: 'item-1',
+                    type: 'chart',
+                    class: "fx-timeseries-ecample",
+                    //needed if layout = injected
+                    container: "#population-1",
+                    config: {
+                        container: "#population-1",
+                        adapter: {
+                            type: "standard",
+                            xDimensions: 'time',
+                            yDimensions: 'item',
+                            valueDimensions: 'value',
+                            seriesDimensions: ['AgeRangeCode']
+                        },
+                        template: {
+                            //"title": "Top 25..."
+                        },
+                        creator: {
+                            chartObj: {
+                                chart: {
+                                    type: "column"
+                                }
+                            }
+                        }
+                    },
+
+                    filter: [
+                        {
+                            "name": "filter",
+                            "parameters": {
+                                "rows": {
+                                    "IndicatorCode": {
+                                        "codes": [
+                                            {
+                                                "uid": "UNECA_ClassificationOfActivities",
+                                                "codes": [
+                                                    "01010104"
+                                                ]
+                                            }
+                                        ]
+                                    }
+                                }
+                            }
+                        }
+                    ]
+                },
+
+                {
+                    //Time series
+                    id: 'item-2',
+                    type: 'chart',
+                    class: "fx-timeseries-ecample",
+                    //needed if layout = injected
+                    container: "#population-2",
+                    config: {
+                        container: "#population-2",
+                        adapter: {
+                            type: "standard",
+                            xDimensions: 'time',
+                            yDimensions: 'item',
+                            valueDimensions: 'value',
+                            seriesDimensions: ['GenderCode']
+                        },
+                        template: {
+                            //"title": "Top 25..."
+                        },
+                        creator: {
+                            chartObj: {
+                                chart: {
+                                    type: "column"
+                                }
+                            }
+                        }
+                    },
+
+
+                    filter: [
+                        {
+                            "name": "filter",
+                            "parameters": {
+                                "rows": {
+                                    "IndicatorCode": {
+                                        "codes": [
+                                            {
+                                                "uid": "UNECA_ClassificationOfActivities",
+                                                "codes": [
+                                                    "01010101"
+                                                ]
+                                            }
+                                        ]
+                                    }
+                                }
+                            }
+                        }
+                    ]
+                },
+                {
+                    //Time series
+                    id: 'item-3',
+                    type: 'chart',
+                    class: "fx-timeseries-ecample",
+                    //needed if layout = injected
+                    container: "#population-3",
+                    config: {
+                        container: "#population-3",
+                        adapter: {
+                            type: "standard",
+                            xDimensions: 'time',
+                            yDimensions: 'item',
+                            valueDimensions: 'value',
+                            seriesDimensions: []
+                        },
+                        template: {
+                            //"title": "Top 25..."
+                        },
+                        creator: {}
+                    },
+
+
+                    filter: [
+                        {
+                            "name": "filter",
+                            "parameters": {
+                                "rows": {
+                                    "IndicatorCode": {
+                                        "codes": [
+                                            {
+                                                "uid": "UNECA_ClassificationOfActivities",
+                                                "codes": [
+                                                    "010104"
+                                                ]
+                                            }
+                                        ]
+                                    }
+                                }
+                            }
+                        }
+                    ]
+                },
+
+
+                {
+                    //Time series
+                    id: 'item-4',
+                    type: 'chart',
+                    class: "fx-timeseries-ecample",
+                    //needed if layout = injected
+                    container: "#population-4",
+                    config: {
+                        container: "#population-4",
+                        adapter: {
+                            type: "standard",
+                            xDimensions: 'time',
+                            yDimensions: 'item',
+                            valueDimensions: 'value',
+                            seriesDimensions: []
+                        },
+                        template: {
+                            //"title": "Top 25..."
+                        },
+                        creator: {}
+                    },
+
+
+                    filter: [
+                        {
+                            "name": "filter",
+                            "parameters": {
+                                "rows": {
+                                    "IndicatorCode": {
+                                        "codes": [
+                                            {
+                                                "uid": "UNECA_ClassificationOfActivities",
+                                                "codes": [
+                                                    "010105"
+                                                ]
+                                            }
+                                        ]
+                                    }
+                                }
+                            }
+                        }
+                    ]
+                },
+
+
+
+                {
+                    //Time series
+                    id: 'item-5',
+                    type: 'chart',
+                    class: "fx-timeseries-ecample",
+                    //needed if layout = injected
+                    container: "#population-5",
+                    config: {
+                        container: "#population-5",
+                        adapter: {
+                            type: "standard",
+                            xDimensions: 'time',
+                            yDimensions: 'item',
+                            valueDimensions: 'value',
+                            seriesDimensions: []
+                        },
+                        template: {
+                            //"title": "Top 25..."
+                        },
+                        creator: {
+                            chartObj: {
+                                chart: {
+                                    type: "column"
+                                }
+                            }
+                        }
+                    },
+
+
+                    filter: [
+                        {
+                            "name": "filter",
+                            "parameters": {
+                                "rows": {
+                                    "IndicatorCode": {
+                                        "codes": [
+                                            {
+                                                "uid": "UNECA_ClassificationOfActivities",
+                                                "codes": [
+                                                    "010103"
+                                                ]
+                                            }
+                                        ]
+                                    }
+                                }
+                            }
+                        }
+                    ]
+                },
+
+
+
+                {
+                    //Time series
+                    id: 'item-6',
+                    type: 'chart',
+                    class: "fx-timeseries-ecample",
+                    //needed if layout = injected
+                    container: "#population-6",
+                    config: {
+                        container: "#population-6",
+                        adapter: {
+                            type: "standard",
+                            xDimensions: 'time',
+                            yDimensions: 'item',
+                            valueDimensions: 'value',
+                            seriesDimensions: []
+                        },
+                        template: {
+                            //"title": "Top 25..."
+                        },
+                        creator: {}
+                    },
+
+
+                    filter: [
+                        {
+                            "name": "filter",
+                            "parameters": {
+                                "rows": {
+                                    "IndicatorCode": {
+                                        "codes": [
+                                            {
+                                                "uid": "UNECA_ClassificationOfActivities",
+                                                "codes": [
+                                                    "010101"
+                                                ]
+                                            }
+                                        ]
+                                    }
+                                }
+                            }
+                        }
+                    ]
+                }
+
+
+
+
+
+
+            ]
+
+
+        },
+
+
+        
+
+
+
+
+
+
+
+
+
         "education": {
             //data cube's uid
             uid: "UNECA_Education",
