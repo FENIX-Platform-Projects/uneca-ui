4fa341bc3b7fc1662ffc3f3157a145af6020542f
fabriziofenix <fabrizio.castelli@fao.org>
2015-09-02 15:16:30 +0200

    modules

--- i18n/root/modules.js
+++ i18n/root/modules.js
@@ -16,64 +16,54 @@ define({
         },
         {
             "title": {
-                "EN": "Satellite Data Acquisition"
+                "EN": "Trade Flows"
             },
             "body": {
-                "EN": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac nisl ultricies, suscipit est ac, euismod neque. Duis a vulputate orci. Nulla mattis neque metus, eget efficitur quam rutrum vitae. Integer at leo dolor. Curabitur sapien mi, scelerisque eget lacinia id, efficitur vel neque. Integer quis dapibus ligula, id fringilla enim. Duis sed purus nec lorem gravida accumsan. Phasellus ut hendrerit risus, nec commodo dolor. "
+                "EN": "Analysis of the trade flows at regional and national levels and for comparing the behaviors of regional and international import and export dynamics over time."
             },
-            "link": "http://168.202.28.57:8080/geobricks/#en/download/modis",
-            "img": "http://fenixrepo.fao.org/cdn/images/modules/dataAcquisition/remote-acqui.png"
+            "link": "http://fenix.fao.org/demo/cstat/afg/trade.html",
+            "img": "http://fenixrepo.fao.org/cdn/images/modules/trade_AFG/trade_AFG.png"
         },
         {
             "title": {
-                "EN": "Crop Statistics"
+                "EN": "Environmental Monitoring"
             },
             "body": {
-                "EN": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac nisl ultricies, suscipit est ac, euismod neque. Duis a vulputate orci. Nulla mattis neque metus, eget efficitur quam rutrum vitae. Integer at leo dolor. Curabitur sapien mi, scelerisque eget lacinia id, efficitur vel neque. Integer quis dapibus ligula, id fringilla enim. Duis sed purus nec lorem gravida accumsan. Phasellus ut hendrerit risus, nec commodo dolor. "
+                "EN": "Monitoring of environmental indicators like rainfall, temperature, evapo-traspiration and vegetation to detect critical conditions for the agriculture and provide information on the are affected by severe water shortages that could potentially negatively affect the agricultural production."
             },
-            "link": "http://168.202.28.214:7070/fnx_maps_playground/index.html#early_warning/en",
-            "img": "http://fenixrepo.fao.org/cdn/images/modules/cropStatistics/early.png"
+            "link": "http://fenix.fao.org/demo/cstat/afg/environmental.html",
+            "img": "http://fenixrepo.fao.org/cdn/images/modules/env_AFG/environmental_AFG.png"
         },
         {
             "title": {
-                "EN": "Water Management"
+                "EN": "Climate and Vegetation Monitoring Kenya ( Timeseries )"
             },
             "body": {
-                "EN": "<a href>Lorem</a> ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac nisl ultricies, suscipit est ac, euismod neque. Duis a vulputate orci. Nulla mattis neque metus, eget efficitur quam rutrum vitae. Integer at leo dolor. Curabitur sapien mi, scelerisque eget lacinia id, efficitur vel neque. Integer quis dapibus ligula, id fringilla enim. Duis sed purus nec lorem gravida accumsan. Phasellus ut hendrerit risus, nec commodo dolor. "
+                "EN": "Compared time series "
             },
-            "link": "http://168.202.28.214:7070/fnx_maps_playground/index.html#morocco/en",
-            "img": "http://fenixrepo.fao.org/cdn/images/modules/waterManagement/waterTile.png"
+            "link": "http://fenixapps.fao.org/repository/js/FENIX/fenix-map-js/demo/projects/countrystat/scatter_kenya_timeserie.html",
+            "img": "http://fenixrepo.fao.org/cdn/images/modules/monitoring/kenya/timeseries/kenya_monitoring_timeseries.png"
         },
         {
             "title": {
-                "EN": "Trade"
+                "EN": "Climate and Vegetation Monitoring Kenya ( Analysis )"
             },
             "body": {
-                "EN": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac nisl ultricies, suscipit est ac, euismod neque. Duis a vulputate orci. Nulla mattis neque metus, eget efficitur quam rutrum vitae. Integer at leo dolor. Curabitur sapien mi, scelerisque eget lacinia id, efficitur vel neque. Integer quis dapibus ligula, id fringilla enim. Duis sed purus nec lorem gravida accumsan. Phasellus ut hendrerit risus, nec commodo dolor. "
+                "EN": "Combined Analysis "
             },
-            "link": "http://fenixapps.fao.org/repository/js/fenix-map-js/uae_demo/projects/faostat/trade",
-            "img": "http://fenixrepo.fao.org/cdn/images/modules/trade/trade.jpg"
+            "link": "http://fenixapps.fao.org/repository/js/FENIX/fenix-map-js/demo/projects/countrystat/scatter_kenya_rainfall_ndvi.html",
+            "img": "http://fenixrepo.fao.org/cdn/images/modules/monitoring/kenya/analysis/kenya_monitoring_analyis.png"
         },
         {
             "title": {
                 "EN": "Monitoring"
             },
             "body": {
-                "EN": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac nisl ultricies, suscipit est ac, euismod neque. Duis a vulputate orci. Nulla mattis neque metus, eget efficitur quam rutrum vitae. Integer at leo dolor. Curabitur sapien mi, scelerisque eget lacinia id, efficitur vel neque. Integer quis dapibus ligula, id fringilla enim. Duis sed purus nec lorem gravida accumsan. Phasellus ut hendrerit risus, nec commodo dolor. "
+                "EN": "Climate and vegetation monitoring "
+
             },
             "link": "http://fenixapps.fao.org/repository/monitoring",
-            "img": "http://fenixrepo.fao.org/cdn/images/modules/monitoring/monitoring.jpg"
-        },
-        {
-            "title": {
-                "EN": "Poverty Analysis"
-            },
-            "body": {
-                "EN": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac nisl ultricies, suscipit est ac, euismod neque. Duis a vulputate orci. Nulla mattis neque metus, eget efficitur quam rutrum vitae. Integer at leo dolor. Curabitur sapien mi, scelerisque eget lacinia id, efficitur vel neque. Integer quis dapibus ligula, id fringilla enim. Duis sed purus nec lorem gravida accumsan. Phasellus ut hendrerit risus, nec commodo dolor. "
-            },
-            "link": "http://fenixapps.fao.org/repository/js/fenix-map-js/1.1/projects/index.html#scatter/poverty_analysis",
-            "img": "http://fenixrepo.fao.org/cdn/images/modules/povertyAnalysis/poverty.jpg"
-
+            "img": "http://fenixrepo.fao.org/cdn/images/modules/monitoring/general/monitoring.jpg"
         }
     ]
 });
