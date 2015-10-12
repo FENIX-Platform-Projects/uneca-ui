3d7d1607343a7ec2d1cab51c32ea392e47721235
danielefenix <daniele.salvatore@fao.org>
2015-10-12 11:40:22 +0200

    Titles harmonization

--- i18n/root/profile.js
+++ i18n/root/profile.js
@@ -1,6 +1,6 @@
 /*global define*/
 define({
-    "title": "Profile",
+    "title": "Country Profile",
     "text" : "Change me in 118n/root/profile.js",
     lateral_menu_title : 'Select a domain'
 });
