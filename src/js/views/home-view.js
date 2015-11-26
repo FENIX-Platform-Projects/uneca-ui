c0c00d9c6ca5b464ac3148a6e7a41985c034c8cb
fabriziofenix <fabrizio.castelli@fao.org>
2015-11-26 18:44:26 +0100

    ..

--- src/js/views/home-view.js
+++ src/js/views/home-view.js
@@ -23,7 +23,7 @@ define([
     'projector',
     'tweenMax',
     'amplify'
-], function (View, C, Q, E, DATA,template, i18nLabels, Handlebars, WDSClient, chartTemplate) {
+], function (View, C, Q, E, DATA, template, i18nLabels, Handlebars, WDSClient, chartTemplate) {
 
     'use strict';
 
@@ -93,254 +93,20 @@ define([
 
             this.data = JSON.parse(DATA);
 
+            for(var k in this.data['import']) {
+                this.data['import'][k].length = 9;
+                this.data['export'][k].length = 9;
+
+            };
 
             var series1GDP = [5.8, 6, 5.4, 3.4, 5.7, 2.8, 6.7, 3.5, 3.9];
             var series2GDP = [5.6, 6.9, 10.7, 9.7, 8, 9.2, 9.2, 7, 7.2];
 
-            var dataTrade = this._prepareDataForTradeCharts()
+            var dataTrade = this._prepareDataForTradeCharts();
+            console.warn('***************************************')
 
-            var tradeEXP_Africa = [68462.5104220494,
-                9050.6287310754,
-                4115.0177205069,
-                16013.4677062515,
-                9200.8443261552,
-                16013.4677062515,
-                11962.8622821194,
-                32107.026934404,
-                7817.6145332362,
-                513492.712089327
-            ];
-
-            var tradeEXP_AMU  = [8549.0820712788,
-                5523.6582642259,
-                359.3222079135,
-                1353.0396394817,
-                448.6223826144,
-                1353.0396394817,
-                1139.4696250704,
-                146.6742921518,
-                778.725838483,
-                143574.65932264
-            ];
-
-            var tradeEXP_CAEMC = [1768.9245073853,
-                52.2110401204,
-                635.4886141976,
-                419.9764051568,
-                754.5348933603,
-                419.9764051568,
-                770.2081692759,
-                643.1666983442,
-                134.2816411206,
-                40495.3215939978
-            ];
-
-            var tradeEXP_COMESA = [20127.030266817,
-                423.1839998335,
-                2705.5278529663,
-                11936.8401903878,
-                2929.3200448406,
-                11936.8401903878,
-                9043.9308542918,
-                4972.2696247999,
-                6337.9443900915,
-                129012.727248329
-            ]
-
-            var tradeEXP_ECCAS = [5543.0231074565,
-                60.9879597404,
-                659.6638061102,
-                393.0657007599,
-                794.0378825135,
-                448.9599476145,
-                803.8584945159,
-                4285.653781385,
-                143.756774448,
-                109809.073380668,
-            ]
-
-            var tradeEXP_ECOWAS = [20127.030266817,
-                423.1839998335,
-                2705.5278529663,
-                11936.8401903878,
-                2929.3200448406,
-                11936.8401903878,
-                9043.9308542918,
-                4960.2623594103,
-                6337.9443900915,
-                129012.727248329
-            ];
-
-            var tradeEXP_FrancZone = [9582.7106344252,
-                323.0399059275,
-                1830.4641135651,
-                6392.3649015739,
-                2072.8507605572,
-                6392.3649015739,
-                4569.8833684967,
-                977.3740234641,
-                2738.9606436977,
-                59526.9478307125
-            ];
-
-            var tradeEXP_SADC = [29334.3371368016,
-                557.1697869014,
-                296.6365162176,
-                1889.7350756463,
-                4289.5425397316,
-                1889.7350756463,
-                752.4018135932,
-                24549.2445782135,
-                436.8348595936,
-                183082.782536727
-            ];
-
-            var tradeEXP_WAEMU = [7810.1374870042,
-                270.2167212327,
-                1194.5321075021,
-                5971.7666043059,
-                1317.8724753314,
-                5971.7666043059,
-                3799.2298707138,
-                331.2206208531,
-                2604.2391519778,
-                18985.4785552384
-            ];
-
-            var tradeEXP_WORLD = [569221.298069115,
-                141779.609446006,
-                20439.955940727,
-                123931.704086158,
-                47918.8064727643,
-                106753.248023692,
-                61280.7513025198,
-                179817.059969213,
-                40656.8743238408,
-                18227586.1786803
-                ];
-
-
-            var tradeIMP_AFRICA = [69946.042288494,
-                8188.5996355991,
-                1806.185021867,
-                20981.7110758055,
-                5958.2878391665,
-                20981.7110758055,
-                4766.1382242821,
-                29733.8622994837,
-                8230.681238622,
-                563407.593824328
-            ];
-
-
-            var tradeIMP_AMU = [8672.1971005844,
-                5518.79845265,
-                51.6964806163,
-                302.0366234484,
-                61.1571265982,
-                302.0366234484,
-                191.0742899471,
-                584.6082627025,
-                139.3536230232,
-                152926.209887509
-            ];
-
-            var tradeIMP_CAEMC = [3744.7370570706,
-                395.8516836417,
-                699.8914990901,
-                2176.5183041289,
-                726.5988529588,
-                2176.5183041289,
-                2014.377312146,
-                343.9805214413,
-                1314.4640288759,
-                21725.4008538638,
-            ];
-
-            var tradeIMP_COMESA = [17308.2378096072,
-                1262.4555476581,
-                372.2630642216,
-                13120.3855609623,
-                404.6874716023,
-                13120.3855609623,
-                6922.7203281071,
-                2108.4409068757,
-                6550.2391294351,
-                143850.234412651
-            ];
-
-            var tradeIMP_ECCAS = [1444.5528911846,
-                495.002458648,
-                834.6948880192,
-                2417.1038549027,
-                878.2628188526,
-                2417.1038549027,
-                2279.2695633838,
-                4737.0342547029,
-                1444.5528911846,
-                52003.6376049377
-            ];
-
-            var tradeIMP_ECOWAS = [17308.2378096072,
-                1262.4555476581,
-                372.2630642216,
-                13120.3855609623,
-                404.6874716023,
-                13120.3855609623,
-                6922.7203281071,
-                2108.4409068757,
-                6550.2391294351,
-                139888.623031967
-            ];
-
-
-            var tradeIMP_FrancZONE = [12113.8904495556,
-                1028.2116264704,
-                848.4537697829,
-                9130.4912673116,
-                886.1226264463,
-                9130.4912673116,
-                5009.8840011638,
-                834.7427275076,
-                4161.4063168953,
-                59537.3986036757
-            ];
-
-            var tradeIMP_SADC = [32674.1406455378,
-                186.7395670035,
-                662.9535387894,
-                5277.1467967585,
-                4669.6950590901,
-                5277.1467967585,
-                837.8145188296,
-                24426.0313062133,
-                172.0415522188,
-                191870.60574347
-            ];
-
-            var tradeIMP_WAMU = [8336.468109405,
-                631.2621531594,
-                148.5465122113,
-                6953.9344376974,
-                159.508015006,
-                6953.9344376974,
-                6953.9344376974,
-                470.404671378,
-                2846.9037625341,
-                37561.4368568348
-            ];
-
-            var tradeIMP_WORLD = [609773.793457011,
-                126541.689410851,
-                43065.4246888646,
-                137783.347944203,
-                119285.643243957,
-                137783.347944203,
-                63040.3755886891,
-                237374.054658063,
-                19925.3412007829,
-                18685081.8269679
-            ]
+            console.warn(dataTrade, 'DATAAA');
+            console.warn('***************************************')
 
             $('#chart1').highcharts($.extend(true, {}, chartTemplate, {
                 title: {
@@ -349,16 +115,7 @@ define([
                 credits: {
                     enabled: false
                 },
-               /* colors: [ //Colori delle charts
-                    '#005493',
-                    '#005493',
-                    '#744490',
-                    '#E10079',
-                    '#2D1706',
-                    '#F1E300',
-                    '#F7AE3C',
-                    '#DF3328'
-                ],*/
+
                 xAxis: {
                     categories: [
                         '2006',
@@ -404,26 +161,26 @@ define([
                     }]
             }));
 
-            $('#chart2').highcharts($.extend(true, {}, chartTemplate, {
+            $('#chart4').highcharts($.extend(true, {}, chartTemplate, {
                 chart: {
                     type: 'column'
                 },
                 title: {
-                    text: 'Trade'
+                    text: 'Import of Goods and services in Africa'
                 },
                 credits: {
                     enabled: false
                 },
-                colors: [ //Colori delle charts
-                    '#a62d3e',
-                    '#986e2e',
-                    '#744490',
-                    '#E10079',
-                    '#2D1706',
-                    '#F1E300',
-                    '#F7AE3C',
-                    '#DF3328'
-                ],
+                /*  colors: [ //Colori delle charts
+                 '#a62d3e',
+                 '#986e2e',
+                 '#744490',
+                 '#E10079',
+                 '#2D1706',
+                 '#F1E300',
+                 '#F7AE3C',
+                 '#DF3328'
+                 ],*/
                 xAxis: {
                     categories: [
                         'AFRICA',
@@ -434,17 +191,16 @@ define([
                         'ECOWAS',
                         'FRANC ZONE',
                         'SADC',
-                        'WAEMU',
-                        'WORLD']
+                        'WAEMU']
                 },
                 yAxis: {
                     min: 0,
                     title: {
-                        text: 'Trade, World (value in AED)'
+                        text: 'Millions of WSD'
                     }
                 },
                 tooltip: {
-                    pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.percentage:.0f}%)<br/>',
+                    pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> <br/>',
                     shared: true
                 },
                 plotOptions: {
@@ -452,47 +208,82 @@ define([
                         stacking: 'normal'
                     }
                 },
-                series: [{
-                    name: 'Re-Export',
-                    data: [31444008427.00, 41124922234.00, 50696679981.00, 69515225632.00, 97043197533.00, 95580167401.00, 128338414920.29, 162844575681.00, 147693366837.24, 185863253742.03, 210842814591.00]
-                }, {
-                    name: 'Export',
-                    data: [7535955504.00, 8649496688.00, 10588599116.00, 14615221779.00, 16462607861.00, 29232285857.00, 36262324842.00, 60359055129.00, 65278896533.54, 83077687322.91, 114038287878.38]
-                },
-                    {
-                        name: 'Import',
-                        data: [147775800078.00, 202896419979.00, 247589715083.00, 291048964687.00, 388356836394.00, 565719823370.00, 447393840482.09, 485413921745.90, 602757314852.26, 667520204394.26, 685068106621.00]
-                    }]
+                series: dataTrade.import
             }));
 
-            $('#chart3').highcharts($.extend(true, {}, chartTemplate, {
+            $('#chart2').highcharts($.extend(true, {}, chartTemplate, {
                 chart: {
                     type: 'column'
                 },
                 title: {
-                    text: 'Arable and Permanent Crop Land Area, 1000 ha'
+                    text: 'Export of Goods and services in Africa'
                 },
                 credits: {
                     enabled: false
                 },
-                colors: [ //Colori delle charts
-                    '#005493',
-                    '#005493',
-                    '#744490',
-                    '#E10079',
-                    '#2D1706',
-                    '#F1E300',
-                    '#F7AE3C',
-                    '#DF3328'
-                ],
+                /*  colors: [ //Colori delle charts
+                 '#a62d3e',
+                 '#986e2e',
+                 '#744490',
+                 '#E10079',
+                 '#2D1706',
+                 '#F1E300',
+                 '#F7AE3C',
+                 '#DF3328'
+                 ],*/
                 xAxis: {
-                    categories: ['2010',
+                    categories: [
+                        'AFRICA',
+                        'AMU',
+                        'CAEMC',
+                        'COMESA',
+                        'ECCAS',
+                        'ECOWAS',
+                        'FRANC ZONE',
+                        'SADC',
+                        'WAEMU']
+                },
+                yAxis: {
+                    min: 0,
+                    title: {
+                        text: 'Millions of WSD'
+                    }
+                },
+                tooltip: {
+                    pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> <br/>',
+                    shared: true
+                },
+                plotOptions: {
+                    column: {
+                        stacking: 'normal'
+                    }
+                },
+                series: dataTrade.export
+            }));
+
+            $('#chart3').highcharts($.extend(true, {}, chartTemplate, {
+                title: {
+                    text: 'Gender parity index'
+                },
+                credits: {
+                    enabled: false
+                },
+
+                xAxis: {
+                    categories: ['2006',
+                        '2007',
+                        '2008',
+                        '2009',
+                        '2010',
                         '2011',
                         '2012',
                         '2013']
                 },
+                crosshair: true,
+
                 yAxis: {
-                    min: 0
+                    min: 0,
+                    max: 1
                 },
                 tooltip: {
                     pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b><br/>',
@@ -500,32 +291,110 @@ define([
                 },
                 plotOptions: {
                     column: {
-                        stacking: 'normal'
+                        pointPadding: 0.2,
+                        borderWidth: 0
                     }
                 },
                 series: [{
-                    name: 'UNECA',
-                    data: [92.53, 88.50, 76.63, 77.24]
-                }]
+                    name: 'Gender parity index in primary',
+                    data: this.data.education.primary
+                },
+                    {
+                        name: 'Gender parity index in secondary',
+                        data: this.data.education.secondary
+                    }
+                ]
             }));
         },
 
-        _prepareDataForTradeCharts : function() {
+        _traspostMatrix: function (key, type) {
+
+            var result = [];
+
+            var map = {
+                "AFRICA": 0,
+                "AMU": 1,
+                "CAEMC": 2,
+                "COMESA": 3,
+                "ECCAS": 4,
+                "ECOWAS": 5,
+                "FRANC_ZONE": 6,
+                "SADC": 7,
+                "WAEMU": 8
+            };
+            var index = map[key];
+
+            for (var field in this.data.trade[type]) {
+                if(type == 'import' && key != 'AFRICA' && field == 'AMU') {
+                    console.log('field: ' + field + ' where origin is ' + key);
+
+
+                    console.log('index: ' + index);
+
+
+                    console.log(this.data.trade[type][field], 'fino a field')
+                    console.log(this.data.trade[type][field][index], 'inex')
+                }
+
+                result.push(this.data.trade[type][field][index])
+            }
+
+           return result;
+
+        },
+
+        _prepareDataForTradeCharts: function () {
 
             var result = {};
 
             result['import'] = []
             result['export'] = [];
 
-            for(var key in this.data.trade.import) {
-                result['import'].push({'name':key,'data': this.data.trade.import[key]})
-            }
+            var semiResult = {'export':{},'import':{}};
 
 
             for(var key in this.data.trade.export) {
-                result['export'].push({'name':key,'data': this.data.trade.export[key]})
+                semiResult['export'][key] = this._traspostMatrix(key, 'export');
+                semiResult['import'][key] = this._traspostMatrix(key, 'import');
             }
 
+            console.log(semiResult);
+
+
+            this.data.trade = semiResult;
+            for (var key in this.data.trade.import) {
+
+/*
+                this.data.trade.import[key] = this._traspostMatrix(key, 'import');
+*/
+
+
+                for (var i = 0; i < this.data.trade.import[key].length; i++) {
+
+                    this.data.trade.import[key][i] = parseFloat(this.data.trade.import[key][i].toFixed(2));
+                }
+
+                result['import'].push({'name': key, 'data': this.data.trade.import[key]})
+            }
+
+
+            for (var key in this.data.trade.export) {
+
+
+/*
+                this.data.trade.export[key] = this._traspostMatrix(key, 'export');
+*/
+
+                for (var i = 0; i < this.data.trade.export[key].length; i++) {
+
+                    this.data.trade.export[key][i] = parseFloat(this.data.trade.export[key][i].toFixed(2));
+                }
+                result['export'].push({'name': key, 'data': this.data.trade.export[key]})
+            }
+
+
+            console.log(result)
+            return result;
 
 
         },
