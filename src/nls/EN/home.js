70f63622a71e1704d06e75bdef82f90f61a4ad63
tanzm <Tanzila.Mohammad@fao.org>
2016-11-15 10:37:24 +0000

    Home page tabbed charts section

--- src/nls/EN/home.js
+++ src/nls/EN/home.js
@@ -6,7 +6,7 @@ define(function () {
         gdp_ppp :"GDP PPP",
         gdp_ppp_chart_title :"GDP, PPP (current international $ billion)",
         gdp_capita :"GDP per capita",
-        gdp_capita_chart_title :"GDP per capita (current, UDS)",
+        gdp_capita_chart_title :"GDP per capita (current, USD)",
         gdp_growth :"GDP growth",
         gdp_growth_chart_title :"Real GDP growth (%)",
         inflation :"Inflation",
